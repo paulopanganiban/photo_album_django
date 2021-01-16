7779632dccf52c2ccdc7224f6fdc465672d4d1a3
John Paulo Panganiban <olopangs@gmail.com>
2021-01-16 13:08:38 +0800

    fixed def save users.models.py

--- photo_album_project/users/models.py
+++ photo_album_project/users/models.py
@@ -18,8 +18,8 @@ class Profile(models.Model):
         #imagefield pip install pillow stuff
         #19:37
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:

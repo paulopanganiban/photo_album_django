d14ea6b8e7c51a0849a114bbe9eca6971b3f1bc1
John Paulo Panganiban <olopangs@gmail.com>
2021-02-18 06:54:50 +0800

    make it better

--- config/base.py
+++ config/base.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 # pip install python-decouple
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production

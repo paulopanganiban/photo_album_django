fd8a449575cb9f403e0beafccc5bef849da26907
John Paulo Panganiban <olopangs@gmail.com>
2021-03-27 11:06:25 +0800

    make it better

--- config/development.py
+++ config/development.py
@@ -1,7 +1,6 @@
 from .base import *
 # development settings
 # conda activate djphoto2.1
-from .base import *
 import os
 # Override base.py settings here
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
